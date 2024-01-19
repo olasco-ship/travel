92bd2c8770af03511320bb25b23c2fd490ce2d21
Nathaniel-promise1 <promisenath@gmail.com>
2024-01-19 11:14:14 +0100

    added the other visa pages

---
+++ visitor_visa.php
@@ -0,0 +1,286 @@
+<head>
+	<title>Home One || Migrate || Immigration Service Responsive HTML 5 Template</title>
+    <?php include('./partials/header.php') ?>
+</head>
+
+<body>
+<div class="boxed_wrapper">
+
+<div class="preloader"></div>
+
+<!-- Main header -->
+<?php include('./partials/nav.php') ?>
+
+
+<section class="mt-4">
+        <div class="first">
+            <a href="#"><span class="text">Visas</span></a>
+            <span class="icon"><i class="bi bi-chevron-right"></i></span>
+            <a href="#"><span class="text">United States</span></a> 
+            <span class="icon"><i class="bi bi-chevron-right"></i></span>
+            <a href="#"><span class="text">List</span></a>
+            <span class="icon"><i class="bi bi-chevron-right"></i></span>
+            <a href="#"><span class="text">Visitor Visa</span></a>
+        </div>
+</section>
+
+<section>
+    <nav class="navbar bg-primary1 navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div>
+        <a style="color: white;" class="navbar-brand" href="#">General Information</a>
+        <a style="color: white;" class="navbar-brand" href="#">Pricing</a>
+        <a style="color: white;" class="navbar-brand" href="#">How It Works</a>
+        <a style="color: white;" class="navbar-brand" href="#">Success Rate</a>
+        <a style="color: white;" class="navbar-brand" href="#">Required Documents</a>
+    </div>
+    </nav>
+</section>
+
+<section class="container mt-5">
+<div class="header-top1">
+        <div>
+            <div class="outer-box clearfix">
+                <div class="header-top-left pull-left">
+                   <h1>Visitor Visa</h1>
+                </div>
+                <div class="header-top-right clearfix pull-right">
+                    <div class="header-social-link style2">
+                        <ul>
+                            <li>
+                                 <a href="#">
+                                 <button type="button" class="btn btn-primary" href="#">Start an Application</button> 
+                                 </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                <button type="button" class="btn btn-outline-primary">Check My Eligibility</button> 
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>  
+            </div>
+        </div>
+    </div> 
+
+    <div>
+        <h6>Would you like to visit the U.S. for business or recreational purposes?</h6><br>
+    </div>
+</section>
+
+<section class="container">
+    <div>
+        <h6><span>This program is for people who are not from visa-exempt countries and want to enter the United States temporarily for business (B-1), for tourism (B-2), or for a combination of both purposes (B-1/B-2).</span></h6><br>
+        <h6><span>Citizens of visa-exempt countries can travel to the U.S. without a visa but may still need an Electronic Travel Authorization (eTA), which is different from a Visitor Visa. </span></h6><br>
+        <h6><span>We recommend starting the application process as early as possible, as travel restrictions and lengthy consulate wait times for visa appointments may impact your travel plans.</span></h6>
+        <h6><span>Check your eligibility now!</span></h6>
+        
+        <div>
+            <ol type="1">
+                <li>
+                    <u>Use our free eligibility checker</u> to quickly see if you are from a visa-exempt country, or if you need a Visitor Visa.
+                </li>
+
+                <li>
+                    Or, start your application by creating an account. It is free and will check if you are eligible. With an account, you can schedule a call with one of our immigration experts.
+                </li>
+            </ol>
+        </div>
+
+    </div>
+</section>
+
+<section class="bg-red pys-10">
+    <div class="container columns-3">
+        <div>
+            <h3>Pricing</h3>
+            <div class="pricing">
+                US$
+                <span>249</span>
+            </div>
+            <p>+ Government Fees</p>
+        </div>
+
+        <div>
+            <ul>
+                <li class="lio"><i class="bi bi-check2"></i><span>Quick and easy online application</span></li>
+                <li class="lio"><i class="bi bi-check2"></i><span>Preparation of submission package</span></li>
+                <li class="lio"><i class="bi bi-check2"></i><span>Expert review and representation</span></li>
+                <li class="lio"><i class="bi bi-check2"></i><span>$1 million in insurance</span></li>
+                <li class="lio"><i class="bi bi-check2"></i><span>Secure encrypted document storage</span></li>
+            </ul>
+        </div>
+        <div>
+            <div class="img-holder">
+                <a href="#">
+                    <button type="button" class="btn btn-primary" href="#">Start an Application</button> 
+                </a>
+                <a href="#">
+                    <button type="button" class="btn btn-outline-primary">Check My Eligibility</button> 
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container pxs-4">
+    <h3 class="texts-2x">How It Works</h3>
+    <ul class="ulw">
+        <li class="io">
+            <span class="num">
+                1
+            </span>
+            <span class="flex-1w">
+                <div class="heeder">Create An Account</div>
+                <p class="para">Do an eligibility check, or pick a visa, or just sign up directly to create an account. Then you can see all the documents needed, questions you must answer, and fee payments for our services and to the government. If you have any questions, it is easy to contact our sales team or to schedule a call.</p>
+            </span>
+        </li>
+
+        <li class="io">
+            <span class="num">
+                2
+            </span>
+            <span class="flex-1w">
+                <div class="heeder">Eligibility Confirmation Before Payment</div>
+                <p>First you will answer a few questions to ensure you are eligible for the visa. Only if you are eligible will the payment unlock. Making payment assigns an immigration lawyer or regulated consultant to your application. We are transparent in our process and pricing, and clearly list all the fees and processing times for your visa. Read our Transparency Pledge. We use Stripe for credit or debit card payment processing, which is secure and encrypted.</p>
+            </span>
+        </li>
+
+        <li class="io">
+            <span class="num">
+                3
+            </span>
+            <span class="flex-1w">
+                <div class="heeder">Complete Your Application Online</div>
+                <p>You can complete as many of the application tasks you wish before payment. Take your time. Some visas require a lot of information, which requires answering questionnaires and uploading of documents. Our system ensures everything necessary is gathered depending on your situation - preventing errors and omissions - and saving you time.</p>
+            </span>
+        </li>
+
+        <li class="io">
+            <span class="num">
+                4
+            </span>
+            <span class="flex-1w">
+                <div class="heeder">Visa Preparation and Submission</div>
+                <p>Once all the tasks are completed, our experts will review your answers and start preparing your application so that the best case is best presented. Government fees are also due once the application is completed and we can correctly calculate the government fees due. After a final review by you, and by our experts, your application will be submitted! You will have peace of mind that only comes with an accurate application.</p>
+            </span>
+        </li>
+
+        <li class="io">
+            <span class="num">
+                5
+            </span>
+            <span class="flex-1w">
+                <div class="heeder">Approval and Renewals</div>
+                <p>We notify you when your visa is approved! If there are any questions or rejections from the government, we will answer them and resubmit without any further fees from us. We securely store your immigration documents for 7 years and provide alerts well before any expiry dates. Renewing or taking the next step toward citizenship is easy when all your immigration history is in one place.</p>
+            </span>
+        </li>
+    </ul>
+</section>
+
+<section class="text0">
+    <div class="flexs-container">
+        <div class="flexs-item flexs-item-1">
+            <div class="pop">98%</div>
+            <div class="pop2">Success Rate</div>
+        </div>
+        <div class="flexs-item flexs-item-2">
+            <div class="pap"><i class="bi bi-patch-check"></i>Expert Review</div>
+            <div class="pap2">Our experienced, regulated immigration consultants review and finalize all applications.</div>
+        </div>
+        <div class="flexs-item flexs-item-3">
+            <div class="pap"><i class="bi bi-coin"></i>Lower Cost</div>
+            <div class="pap2">Our technology keeps cost much lower than traditional law firms.</div>
+        </div>
+        <div class="flexs-item flexs-item-4">
+            <div class="pap"><i class="bi bi-lightning-charge"></i>Faster and More Accurate</div>
+            <div class="pap2">Our software is changing immigration, but human support is always available.</div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="pax">
+        <h5 class="tex">Required Documents</h5>
+        <p class="tex1">Some documents you will need to provide, and some documents we may prepare for you.</p>
+        <ul>
+            <li class="tex2">No document is required for this visa</li>
+        </ul>
+    </div>
+    <div>
+        <div class="row justify-content-center">
+            <!--Start Single select Country-->
+            <div class="col-6 col-md-4">  
+            </div>
+            <!--End Single select Country-->
+            <!--Start Single select Country-->
+            <div class="col-6 col-md-4">
+                <div class="single-select-country">
+                    <div>
+                        <a href="#">
+                            <button type="button" class="btn btn-primary" href="#">Start an Application</button> 
+                        </a>
+                        <a href="#">
+                            <button type="button" class="btn btn-outline-primary">Check My Eligibility</button> 
+                        </a>
+                    </div>    
+                </div>    
+            </div>
+            <!--End Single select Country-->
+            <!--Start Single select Country-->
+            <div class="col-6 col-md-4">   
+            </div>
+            <!--End Single select Country-->
+        </div>  
+    </div>
+</section>
+
+
+
+
+
+
+<!--Start footer area-->  
+<?php include('./partials/footer.php') ?>
+<!--End footer area-->
+
+
+    <!--   JAVASCRIPT   -->
+    <script src="assets/js/star.js"></script>
+
+        <!-- <script src="assets/js/script.js"></script> -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/jquery.bootstrap-touchspin.js"></script>
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    <script src="assets/js/jquery.countTo.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/jquery.enllax.min.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/jquery.mixitup.min.js"></script>
+    <script src="assets/js/jquery.paroller.min.js"></script>
+    <script src="assets/js/jquery.polyglot.language.switcher.js"></script>
+    <script src="assets/js/map-script.js"></script>
+    <script src="assets/js/nouislider.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/timePicker.js"></script>   
+    <script src="assets/js/validation.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/lazyload.js"></script>
+    <script src="assets/js/scrollbar.js"></script>
+    <script src="assets/js/tilt.jquery.js"></script>
+    <script src="assets/js/jquery.bxslider.min.js"></script>
+    <script src="assets/js/jquery-ui.js"></script>
+    <script src="assets/js/jQuery.style.switcher.min.js"></script>
+    <!-- thm custom script -->
+    <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
