f411e31ce2df5cc486755913f69cdff539df33e2
Nathaniel-promise1 <promisenath@gmail.com>
2024-01-23 14:50:38 +0100

    color changed

--- about-us.php
+++ about-us.php
@@ -27,33 +27,29 @@
 
 
             <p class="smallp about_p"><?php echo $menu['des']; ?>
-                <!-- We are immigrants and/or have experienced the visa application process. We believe the immigration experience could be better - simpler, faster, less expensive and with more confidence in the outcome. -->
+                We are immigrants and/or have experienced the visa application process. We believe the immigration experience could be better - simpler, faster, less expensive and with more confidence in the outcome.
+            </p>  
 
 
-            <!-- </p> -->
-            
-            
-
-
-            <!-- <p class="p_nd smallp"> -->
-               <!--  We believe that generative artificial intelligence (AI) and machine vision has the potential to drastically simplify the visa application process. We are reducing the data entry and document generation steps for both the applicant and our expert reviewers. With the proper use of technology, our goal is to make access to expert immigration advice more accessible since it is easy, low cost, supports multiple languages and is accessible to anyone anywhere with an Internet connection. -->
-            <!-- </p>
-            <p class="p_rd smallp"> -->
-                <!-- Then everyone would be happier. -->
-            <!-- </p>
-            <p class="p_nd smallp"> -->
-                <!-- We believe that generative artificial intelligence (AI) and machine vision has the potential to drastically simplify the visa application process. We are reducing the data entry and document generation steps for both the applicant and our expert reviewers. With the proper use of technology, our goal is to make access to expert immigration advice more accessible since it is easy, low cost, supports multiple languages and is accessible to anyone anywhere with an Internet connection. -->
-            <!-- </p>
-            <p class="p_rd smallp"> -->
-                <!-- Then everyone would be happier. -->
-            <!-- </p> -->
+            <p class="p_nd smallp"> 
+               We believe that generative artificial intelligence (AI) and machine vision has the potential to drastically simplify the visa application process. We are reducing the data entry and document generation steps for both the applicant and our expert reviewers. With the proper use of technology, our goal is to make access to expert immigration advice more accessible since it is easy, low cost, supports multiple languages and is accessible to anyone anywhere with an Internet connection. -->
+            </p>
+            <p class="p_rd smallp">
+               Then everyone would be happier.
+            </p>
+            <p class="p_nd smallp"> 
+                We believe that generative artificial intelligence (AI) and machine vision has the potential to drastically simplify the visa application process. We are reducing the data entry and document generation steps for both the applicant and our expert reviewers. With the proper use of technology, our goal is to make access to expert immigration advice more accessible since it is easy, low cost, supports multiple languages and is accessible to anyone anywhere with an Internet connection. -->
+            </p>
+            <p class="p_rd smallp">
+             Then everyone would be happier. 
+            </p> 
         </p>
             <?php } ?>
         </div>
     </section>
 
 
-    <!-- <section class="about_mid">
+ <section class="about_mid">
         <h3 class="title">SuperVisas <span class="blue">team</span></h3>
         <div class="infos">
         <div class="info">
@@ -189,7 +185,7 @@
             </div>
         </div>
         </div>
-    </section> -->
+    </section>
     <section class="thrid">
         <h3 class="third_title" style="margin-top: 10%; text-align: center !important;">
         Our mission is to provide the easiest, fastest, most accurate <br>and lowest cost immigration service on the planet.
@@ -204,7 +200,8 @@
             <div class="one_third">
                 <h4 class="blue">98%</h4>
                 <p>
-                Current government of Canada visa approval rate                 </p>
+                Current government of Canada visa approval rate                 
+               </p>
             </div>
             <div class="one_third">
                 <h4 class="blue">2</h4>
@@ -238,7 +235,7 @@
             </div>
         </div>
     </section>
-    <!-- <section class="press">
+     <section class="press">
         <h3 class="blue press_title">Press</h3>
         <div class="news">
             <h4>Latent in the news</h4>
@@ -247,7 +244,6 @@
                     <a href="#" class="date">November 2023</a> <br>
                     <a href="" class="company"><span class="rip">Rippling connects Latent to simplify employee travel and immigration</span></a> <a href="#"><span class="rip_icon"><img src="assets/images/resources/rippling.webp" alt=""></span></a>
                 </div>
-<<<<<<< HEAD
                 <div class="each_news">
                     <a href="#" class="date">November 2023</a> <br>
                     <a href="" class="company"><span class="rip">Rippling connects Latent to simplify employee travel and immigration</span></a> <a href="#"><span class="rip_icon"><img src="assets/images/resources/rippling.webp" alt=""></span></a>
@@ -264,11 +260,9 @@
                     <a href="#" class="date">November 2023</a> <br>
                     <a href="" class="company"><span class="rip">Rippling connects Latent to simplify employee travel and immigration</span></a> <a href="#"><span class="rip_icon"><img src="assets/images/resources/rippling.webp" alt=""></span></a>
                 </div>
-=======
->>>>>>> 0de138d7349eaeeeca68ccdc572de82855c05bd4
             </div>
         </div>
-    </section> -->
+    </section> 
     <!--Start footer area-->  
     <?php include('./partials/footer.php') ?>
     <!--End footer area-->
